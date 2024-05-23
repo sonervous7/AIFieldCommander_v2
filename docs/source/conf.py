1ff167115e8b0cf31579fd5af998d16e034bb291
sonervous7 <jakub.buszynski@gmail.com>
2024-05-23 19:04:07 +0200

    Added large data file to repo using Git LFS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,28 +11,34 @@ import sys
 from unittest import mock
 
 import sphinx_pdj_theme
-MOCK_MODULES = ['PyQt5', 'PyQt5.QtWidgets', 'PyQt5.QtGui', 'PyQt5.QtCore']
-sys.modules.update((mod_name, mock.Mock()) for mod_name in MOCK_MODULES)
-sys.path.insert(0, os.path.abspath('../../src'))
 
-project = 'AI Field Commander'
-copyright = '2024, Jakub Buszyński'
-author = 'Jakub Buszyński'
-release = '1.0.0'
+MOCK_MODULES = ["PyQt5", "PyQt5.QtWidgets", "PyQt5.QtGui", "PyQt5.QtCore"]
+sys.modules.update((mod_name, mock.Mock()) for mod_name in MOCK_MODULES)
+sys.path.insert(0, os.path.abspath("../../src"))
+
+project = "AI Field Commander"
+copyright = "2024, Jakub Buszyński"
+author = "Jakub Buszyński"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "sphinx.ext.intersphinx", "sphinx.ext.coverage", "sphinx.ext.inheritance_diagram"]
 
-templates_path = ['_templates']
+autodoc_default_options = {
+    'show-inheritance': True,
+    'inherited-members': True,
+    'member-order': 'bysource',
+}
+
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'renku'
+html_theme = "renku"
 # html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
-html_static_path = ['_static']
+html_static_path = ["_static"]
